51a8918b4cf6585910c04fa0333e53701e3ec2c7
punchitox <fjgarciabasanta@gmail.com>
2022-02-17 13:49:23 +0100

    Cliente controller

--- src/main/java/com/formacionspringboot/app/service/ProductoService.java
+++ src/main/java/com/formacionspringboot/app/service/ProductoService.java
@@ -2,8 +2,6 @@ package com.formacionspringboot.app.service;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-
 import com.formacionspringboot.app.entity.Producto;
 
 
